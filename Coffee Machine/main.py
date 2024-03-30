b41b4aab079da0a8116c2a378712ef0c04d3fcd7
mbonabucya <mbonajames12@gmail.com>
2024-03-30 13:55:50 +0200

    add another version

--- Coffee Machine/main.py
+++ Coffee Machine/main.py
@@ -79,7 +79,7 @@ def make_coffee(drink_name, order_ingredients):
 
 
 print("""
-☕☕☕☕☕☕☕☕☕☕☕☕☕☕☕☕☕☕☕☕
+☕☕☕☕☕☕☕☕☕☕☕☕☕☕☕☕☕☕☕☕☕
 =========== Drink coffee for life =========
 """)
 print("""
@@ -109,3 +109,106 @@ while is_on:
             make_coffee(choice, drink['ingredients'])
     else:
         print("Invalid choice.try again")
+
+# TODO : 1  generate the report
+# TODO : 2 track the repources
+        
+"""MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+def display_menu():
+#   Print the available menu and returns user needed coffee
+  print("The coffee items available:\n")
+  counter =0
+  for item in MENU:
+    counter +=1
+    print(f"{counter}. {item}")
+  print("\n")
+  coffee = input("What would you like? (espresso/latte/cappuccino):")
+  print(f"You would like to take {coffee}")
+  if coffee not in MENU:
+    print("That coffee is not available for now. check menu again")
+  else:
+    return coffee
+
+def turn_off():
+  turn = input("Type 'off' to turn off machine: ").lower()
+  if turn == "off":
+    return True
+  else:
+    return False
+
+def check_resources(coffee):
+#   Check if there is enough resources to make the coffee . return true otherwise false
+  for item in MENU[coffee]["ingredients"]:
+    if MENU[coffee]["ingredients"][item] > resources[item]:
+      print(f"Sorry there is not enough {item}")
+      return False
+    else:
+      return True
+      
+
+def process_coins():
+#   Process the coins and return the total amount
+  print("Please insert coins")
+  quarters = int(input("How many quarters?: "))
+  dimes = int(input("How many dimes?: "))
+  nickles = int(input("How many nickles?: "))
+  pennies = int(input("How many pennies?: "))
+  total = (quarters * 0.25) + (dimes * 0.10) + (nickles * 0.05) + (pennies * 0.01)
+  return total
+    
+
+def check_transaction(coffee, total):
+#   Check if the transaction is successful. return true otherwise false
+  if total < MENU[coffee]["cost"]:
+    print("Sorry that's not enough money. Money refunded.")
+    return False
+  else:
+    change = round(total - MENU[coffee]["cost"], 2)
+    print(f"Here is ${change} in change.")
+    return True
+
+def make_coffee(coffee):
+#   Make the coffee and deduct the resources
+  for item in MENU[coffee]["ingredients"]:
+    resources[item] -= MENU[coffee]["ingredients"][item]
+  print(f"Here is your {coffee}. Enjoy!")
+
+coffee =display_menu()
+check_resources(coffee)
+total = process_coins()
+check_transaction(coffee, total)
+make_coffee(coffee)
+
+"""
